72cecee8ce697e3d730762b1f9da4485ccaedb3d
Paco Navarro <franciscojose.navarro.next@bbva.com>
2020-02-17 15:11:34 +0100

    cambio de texto

--- functions/index.js
+++ functions/index.js
@@ -33,7 +33,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     return admin.database().ref('data').once('value').then((snapShot)=>{
     	const value = snapShot.child('userName').val();
       	if(value !== null) {
-        	agent.add(`Que memoria la tuya!, tu nombre es ${value}`);
+        	agent.add(`Que memoria la tuya, tu nombre es ${value}`);
         }
     });
   }
