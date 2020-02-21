06f6cca473350fcf2af4155a021f64391560b8a1
SERGIO SANTAMARIA FAJARDO <sergio.santamaria.next@bbva.com>
2020-02-21 01:17:59 +0100

    update data

--- functions/index.js
+++ functions/index.js
@@ -93,8 +93,8 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
       const value = snapShot.child(SelectedPlace).val();
       if(value !== null) {
         const distance = calculateTravelCoeficient(userData.room[placeName], value);
-        Object.assign( userData, { placesKnown: { [`${SelectedPlace}`]: true }, room: newPlace, hungry: userData.hungry - distance });
-        updateUser(userId, userData);
+        //Object.assign( userData, { placesKnown: { [`${SelectedPlace}`]: true }, room: newPlace, hungry: userData.hungry - distance });
+        //updateUser(userId, userData);
         return agent.add(`estas en ${placeName}, Quieres viajar a ${SelectedPlace}, y esta a una distancia de ${distance}`);
       } else {
         return agent.add(`Nadie ha oido hablar de ese lugar nunca!`);
