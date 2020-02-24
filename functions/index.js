a535bcd9aec4fa63fb5a90e023a90f32bdf84a0b
Víctor Puebla <victormanuel.puebla@bbva.com>
2020-02-24 08:44:35 +0100

    log full intentRequest payload object

--- functions/index.js
+++ functions/index.js
@@ -20,7 +20,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
  
   function welcomeResponse(agent) {
     console.log('datos del agent: ',agent);
-    console.log('request data: ',JSON.stringify(request.body.originalDetectIntentRequest.payload.data.data));
+    console.log('request payload: ',JSON.stringify(request.body.originalDetectIntentRequest.payload));
     console.log('request user: ',JSON.stringify(request.body.originalDetectIntentRequest.payload.data.data.personEmail));
     console.log('request date: ',JSON.stringify(request.body.originalDetectIntentRequest.payload.data.created));
     agent.add(`Hola aventurero!, no sé si eres un valiente o un inconsciente al saludarme, pero en fin, ya lo descubriremos si estas dispuesto a embarcarte en esta aventura. ¿Quieres comenzar la gesta para convertirte en un ingenioso hidalgo?`);
