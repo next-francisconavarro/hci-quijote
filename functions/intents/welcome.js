fa8138ac0642414322ce5bf3d6b70bb0b47017a0
Paco Navarro <franciscojose.navarro.next@bbva.com>
2020-03-09 09:14:09 +0100

    requestPayload

--- functions/intents/welcome.js
+++ functions/intents/welcome.js
@@ -1,12 +1,25 @@
-function welcomeResponse(request) {
+const emptyPayload = {
+  data: {
+    personEmail: 'nouser'
+  }
+};
+
+function welcomeResponse({ body = {} }) {
     return agent => {
+        const payload = getPayload(body);
         console.log('datos del agent: ',agent);
-        console.log('request payload: ',JSON.stringify(request.body.originalDetectIntentRequest.payload));
-        console.log('request user: ',JSON.stringify(request.body.originalDetectIntentRequest.payload.data.data.personEmail));
-        console.log('request date: ',JSON.stringify(request.body.originalDetectIntentRequest.payload.data.created));
+        console.log('request payload: ', JSON.stringify(payload));
+        console.log('request user: ', JSON.stringify(payload.data.personEmail));
+        console.log('request date: ', JSON.stringify(payload.created));
         
         agent.add('Hola aventurero!, no sé si eres un valiente o un inconsciente al saludarme, pero en fin, ya lo descubriremos si estas dispuesto a embarcarte en esta aventura. ¿Quieres comenzar la gesta para convertirte en un ingenioso hidalgo?');
     };
+}
+
+function getPayload(body) {
+  return body.originalDetectIntentRequest &&
+    body.originalDetectIntentRequest.payload && 
+    body.originalDetectIntentRequest.payload.data || emptyPayload;
 }
 
 module.exports = { welcomeResponse };
