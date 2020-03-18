3e120a8a1bc37c73c0bd825a7279931a86df9031
Paco Navarro <franciscojose.navarro.next@bbva.com>
2020-03-18 21:05:44 +0100

    test

--- functions/utils/time.spec.js
+++ functions/utils/time.spec.js
@@ -1,5 +1,20 @@
 const time = require('./time');
 
 const coordinates = { lat: 39.5137458, lng: -3.0046506};
+const now = Date.now();
+test('Check day detection', () => {
 
-time.isDay({ coordinates });
+  jest.spyOn(Date.prototype, 'sunset').mockImplementation(() => now + 120 * 1000);
+  jest.spyOn(Date.prototype, 'sunrise').mockImplementation(() => now - 120 * 1000);
+
+  expect(time.isDay({ coordinates })).toBe(true);
+  expect(time.isNight({ coordinates })).toBe(false);
+});
+
+test('Check night detection', () => {
+
+  jest.spyOn(Date.prototype, 'sunset').mockImplementation(() => now - 120 * 1000);
+
+  expect(time.isDay({ coordinates })).toBe(false);
+  expect(time.isNight({ coordinates })).toBe(true);
+});
