c25d9d43086794afdd17204ec51c78c703137cfa
Paco Navarro <franciscojose.navarro.next@bbva.com>
2020-03-18 18:06:18 +0100

    time and day

--- functions/intents/rememberUser.js
+++ functions/intents/rememberUser.js
@@ -1,12 +1,18 @@
 const contextDao = require('../dao/context');
 const usersDao = require('../dao/users');
+const { isNight } = require('../utils/time');
 
 function recoverUserName(request) {
     return agent => {
         const userAccount = contextDao.getUserId(request);
         return usersDao.getUserById(userAccount).then(user => {
           if (user) {
-            agent.add(`Que memoria la tuya, tu nombre es ${user.userName}`);
+            agent.add(`Que memoria la tuya, tu nombre es ${user.userName}.`);
+            if (isNight(user)) {
+              agent.add('Parece que es de noche.');
+            } else {
+              agent.add('Hace un día estupendo.');
+            }
           }
         });
     }
---
+++ functions/utils/time.spec.js
@@ -0,0 +1,5 @@
+const time = require('./time');
+
+const coordinates = { lat: 39.5137458, lng: -3.0046506};
+
+time.isDay({ coordinates });
