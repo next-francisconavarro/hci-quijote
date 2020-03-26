4d77d1924bba4e49925f753a65a3fa2e884a5987
Paco Navarro <franciscojose.navarro.next@bbva.com>
2020-03-26 16:07:09 +0100

    user problems

--- functions/dao/context.js
+++ functions/dao/context.js
@@ -1,18 +1,20 @@
 const NO_USER = 'nouser';
 
 function getUserId({ body = {} }) {
-    console.log(`getUserId -> Request body: ${JSON.stringify(body)}`);
-    const userMail = body.originalDetectIntentRequest &&
-      body.originalDetectIntentRequest.payload &&
-      body.originalDetectIntentRequest.payload.data &&
-      body.originalDetectIntentRequest.payload.data.data &&
-      body.originalDetectIntentRequest.payload.data.data.personEmail;
+  const payload = body.originalDetectIntentRequest && body.originalDetectIntentRequest.payload;
+
+    console.log(`getUserId -> Request payload: ${JSON.stringify(payload)}`);
+    const userMail = body.originalDetectIntentRequest && payload &&
+      payload.data &&
+      payload.data.data &&
+      payload.data.data.personEmail;
     
     if (userMail) {
       return userMail.replace(/\.|@.*/g, '');
     }
-    return body.originalDetectIntentRequest.payload.event &&
-      body.originalDetectIntentRequest.payload.event.user  || NO_USER;
+    
+    return payload.data.event &&
+      payload.data.event.user  || NO_USER;
 }
 
 /*function getValidDomain() {
