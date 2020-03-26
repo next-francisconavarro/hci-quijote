4f270625fcf40565a3cef5d0b16419d45b0370b6
Paco Navarro <franciscojose.navarro.next@bbva.com>
2020-03-26 12:59:02 +0100

    images

--- functions/intents/rememberUser.js
+++ functions/intents/rememberUser.js
@@ -1,7 +1,7 @@
 const contextDao = require('../dao/context');
 const usersDao = require('../dao/users');
 const { isNight } = require('../utils/time');
-const { Card } = require('dialogflow-fulfillment');
+const { Card } = require('dialogflow-fulfillment', 'Image');
 
 const imageUrl = 'http://www.commodoreabandonware.com/mod/upload/cmd_es/images/c3/34/ba/61/fe/74/aa/5c/21/72/b4/0b/ea/f5/bd/b1/don_quijote.png';
 
@@ -10,14 +10,15 @@ function recoverUserName(request) {
         const userAccount = contextDao.getUserId(request);
         return usersDao.getUserById(userAccount).then(user => {
           if (user) {
-            agent.add(new Card({
-                title: `Que memoria la tuya, tu nombre es ${user.userName}.`,
-                imageUrl: imageUrl,
-                text: isNight(user) ? 'Hace una noche despejada.' : 'Hace un día estupendo.',
-                buttonText: '?',
-                buttonUrl: 'https://assistant.google.com/'
-              })
-            );
+            // agent.add(new Card({
+            //     title: `Que memoria la tuya, tu nombre es ${user.userName}.`,
+            //     imageUrl: imageUrl,
+            //     text: isNight(user) ? 'Hace una noche despejada.' : 'Hace un día estupendo.',
+            //     buttonText: '?',
+            //     buttonUrl: 'https://assistant.google.com/'
+            //   })
+            // );
+            agent.add(`Que memoria la tuya, tu nombre es ${user.userName}.`);
           }
         });
     }
