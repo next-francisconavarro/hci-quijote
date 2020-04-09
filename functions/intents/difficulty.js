a14874b450b66647fa916f96a33bc78fe19507bf
Paco Navarro <franciscojose.navarro.next@bbva.com>
2020-04-09 12:01:55 +0200

    dificultad

--- functions/intents/difficulty.js
+++ functions/intents/difficulty.js
@@ -4,19 +4,20 @@ const placesDao = require('../dao/places');
 const { Image } = require('dialogflow-fulfillment');
 
 function difficulty(request) {
-    return agent => {
+    return async agent => {
         const difficultyLevel = agent.parameters.difficultyLevel;
-        const userAccount = contextDao.getUserId(request);
+        const userId = contextDao.getUserId(request);
+        const userData = await usersDao.getUserById(userId);
         const preMessage = `Excelente, comenzarás la aventura con dificultad *${difficultyLevel}*.\n`;
-        // return usersDao.addUser(userAccount, ).then(() => {
-        //   // Comenzamos en la biblioteca
-          return placesDao.getPlaceById('biblioteca').then(place => {
-            if(place) {
-              agent.add(new Image(place.media.images[0]));
-              return agent.add(`${preMessage}${place.description}`);
-            }
-          });
-        // });
+
+        await usersDao.updateUser(userId, Object.assign(userData, {difficultyLevel}));
+        // Comenzamos en la biblioteca
+        return placesDao.getPlaceById('biblioteca').then(place => {
+          if(place) {
+            agent.add(new Image(place.media.images[0]));
+            return agent.add(`${preMessage}${place.description}`);
+          }
+        });
     }
 }
 
