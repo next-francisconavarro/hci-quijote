a7ad765ad56177e7ad77f9d4056cad99d6ca3731
Paco Navarro <franciscojose.navarro.next@bbva.com>
2020-04-23 10:35:52 +0200

    ayuda

--- functions/index.js
+++ functions/index.js
@@ -15,6 +15,7 @@ const inventoryIntent = require('./intents/inventory');
 const rememberVisitedIntent = require('./intents/rememberVisited');
 const actionsIntent = require('./intents/actions');
 const difficultyIntent = require('./intents/difficulty');
+const helpIntent = require('./intents/help');
 
 require('./utils/sun');
 
@@ -22,39 +23,7 @@ process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
-  //console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-  //console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-  // // Uncomment and edit to make your own intent handler
-  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function yourFunctionHandler(agent) {
-  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
-  //   agent.add(new Card({
-  //       title: `Title: this is a card title`,
-  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-  //       buttonText: 'This is a button',
-  //       buttonUrl: 'https://assistant.google.com/'
-  //     })
-  //   );
-  //   agent.add(new Suggestion(`Quick Reply`));
-  //   agent.add(new Suggestion(`Suggestion`));
-  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
-  // }
-
-  // // Uncomment and edit to make your own Google Assistant intent handler
-  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function googleAssistantHandler(agent) {
-  //   let conv = agent.conv(); // Get Actions on Google library conv instance
-  //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
-  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
-  // }
-  // // See https://github.com/dialogflow/fulfillment-actions-library-nodejs
-  // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
-
-  // Run the proper function handler based on the matched Dialogflow intent name
   let intentMap = new Map();
   intentMap.set('Default Welcome Intent', welcomeIntent.welcomeResponse(request));
   intentMap.set('Default Fallback Intent', fallbackIntent.fallback);
@@ -65,5 +34,6 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   intentMap.set('Recordar visitados', rememberVisitedIntent.rememberVisited(request));
   intentMap.set('Acciones', actionsIntent.execute(request));
   intentMap.set('difficulty', difficultyIntent.difficulty(request))
+  intentMap.set('Ayuda', helpIntent.execute(request))
   agent.handleRequest(intentMap);
 });
--- functions/utils/difficultyUtils.js
+++ functions/utils/difficultyUtils.js
@@ -16,4 +16,36 @@ function textByDifficulty(text, user) {
   return text;
 }
 
-module.exports = { textByDifficulty };
+function getHelp(user) {
+  const room = (Object.keys(user.room) || [])[0];
+  const actionsDone = user.room.actions || [];
+  const actions = placesDao.getPlaceActions(room);
+  const connectedRooms = placesDao.getConnectedRooms(room);
+  let response = `Estás en ${room}. `;
+
+  if (connectedRooms.length) {
+    response += 'Desde aquí puedes ir a '
+    response += connectedRooms.map(room => `*${room}*`).join(' ') + '. ';
+  }
+
+  if (actionsDone.length < actions.length) {
+    response += 'Quizás puedas hacer algo con: ';
+    
+    if (actions.length > 1) {
+      response += onlyUnique(actions.map(el => el.object && `*${el.object.name}*`)).join(', ') + '...';
+    } else {
+      response += actions[0].object && actions[0].object.name;
+    }
+
+    response += '.';
+  }
+
+
+  return response;
+}
+
+function onlyUnique(values) { 
+  return Array.from(new Set(values));
+}
+
+module.exports = { textByDifficulty, getHelp };
