d1b4e2167ab98267deb15d126c5cfc397c3cf828
Paco Navarro <franciscojose.navarro.next@bbva.com>
2020-11-27 13:12:16 +0100

    maps

---
+++ createMap.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+
+const rawdata = fs.readFileSync('./functions/dao/places.json', 'utf8');
+const places = JSON.parse(rawdata);
+const tree = {
+  name: 'biblioteca',
+  parent: null,
+  children: []
+};
+const parents = ['biblioteca'];
+
+function readNode(parent) {
+  console.log(parent.name);
+  const place = places[parent.name];
+  const connectedRooms = place.connectedRooms || [];
+  parent.children = [];
+  parent.image = place.media.images[0];
+  
+  connectedRooms.forEach(room => {
+    if (!parents.includes(room) && places[room]) {
+      parents.push(room);
+      parent.children.push(readNode({
+        name: room,
+        parent
+      }));
+    }
+  });
+
+  return parent;
+}
+
+function drawNode(node) {
+  let string = `{
+    text: { name: "${node.name}" },
+    image: '${node.image}',
+    children: [
+      ${node.children.length ?
+        node.children.map(drawNode).join('\n') :
+        ''}
+    ]
+  },`;
+
+  return string;
+}
+
+readNode(tree);
+
+let rawOutput = `
+  var chart_config = {
+    chart: {
+        container: "#basic-example",
+        
+        connectors: {
+            type: 'step'
+        },
+        node: {
+            HTMLclass: 'nodeExample1'
+        }
+    },
+    
+    nodeStructure: ${drawNode(tree)}
+  };
+`;
+
+fs.writeFileSync('./map/tree-map.js', rawOutput);
---
+++ map/tree-map.js
@@ -0,0 +1,210 @@
+
+  var chart_config = {
+    chart: {
+        container: "#basic-example",
+        
+        connectors: {
+            type: 'step'
+        },
+        node: {
+            HTMLclass: 'nodeExample1'
+        }
+    },
+    
+    nodeStructure: {
+    text: { name: "biblioteca" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/biblioteca.png',
+    children: [
+      {
+    text: { name: "habitación" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/habitacion.png',
+    children: [
+      {
+    text: { name: "zaguán" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/zaguan.png',
+    children: [
+      {
+    text: { name: "comedor" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/comedor.png',
+    children: [
+      
+    ]
+  },
+{
+    text: { name: "cocina" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/cocina.png',
+    children: [
+      {
+    text: { name: "despensa" },
+    image: 'undefined',
+    children: [
+      
+    ]
+  },
+    ]
+  },
+{
+    text: { name: "portalón" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/portalon.png',
+    children: [
+      {
+    text: { name: "argamasilla" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/argamasilla.png',
+    children: [
+      {
+    text: { name: "calles" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/calles.png',
+    children: [
+      {
+    text: { name: "campos" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/campos.png',
+    children: [
+      {
+    text: { name: "venta" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/venta.png',
+    children: [
+      {
+    text: { name: "recibidor" },
+    image: 'undefined',
+    children: [
+      {
+    text: { name: "distribuidor" },
+    image: 'undefined',
+    children: [
+      {
+    text: { name: "alcoba" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/venta_dormitorio.png',
+    children: [
+      
+    ]
+  },
+{
+    text: { name: "patio" },
+    image: 'undefined',
+    children: [
+      
+    ]
+  },
+    ]
+  },
+{
+    text: { name: "salón" },
+    image: 'undefined',
+    children: [
+      
+    ]
+  },
+    ]
+  },
+{
+    text: { name: "prados" },
+    image: 'undefined',
+    children: [
+      {
+    text: { name: "cancela" },
+    image: 'undefined',
+    children: [
+      
+    ]
+  },
+    ]
+  },
+    ]
+  },
+{
+    text: { name: "acantilado" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/acantilado.png',
+    children: [
+      {
+    text: { name: "bosque" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/bosque_oscuro.png',
+    children: [
+      
+    ]
+  },
+    ]
+  },
+    ]
+  },
+    ]
+  },
+{
+    text: { name: "arco" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/arco.png',
+    children: [
+      {
+    text: { name: "arboleda" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/arboleda.png',
+    children: [
+      {
+    text: { name: "manzano" },
+    image: 'undefined',
+    children: [
+      {
+    text: { name: "pinar" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/bosque.png',
+    children: [
+      {
+    text: { name: "posada" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/posada.png',
+    children: [
+      {
+    text: { name: "muro" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/muro.png',
+    children: [
+      
+    ]
+  },
+{
+    text: { name: "recepción" },
+    image: 'undefined',
+    children: [
+      {
+    text: { name: "escalera" },
+    image: 'undefined',
+    children: [
+      {
+    text: { name: "rellano" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/posada_rellano.png',
+    children: [
+      {
+    text: { name: "bodega" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/bodega.png',
+    children: [
+      
+    ]
+  },
+{
+    text: { name: "dormitorio" },
+    image: 'https://raw.githubusercontent.com/next-francisconavarro/hci-quijote/develop/images/posada_dormitorio.png',
+    children: [
+      
+    ]
+  },
+    ]
+  },
+    ]
+  },
+    ]
+  },
+    ]
+  },
+    ]
+  },
+    ]
+  },
+    ]
+  },
+    ]
+  },
+    ]
+  },
+    ]
+  },
+    ]
+  },
+    ]
+  },
+    ]
+  },
+  };
